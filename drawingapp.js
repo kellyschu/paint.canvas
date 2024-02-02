22eaeeff08372fdd27d881184334a902832a08e0
Kelly Schumacher <kschumacher93@gmail.com>
2024-02-02 17:01:33 -0500

    updates for mobile verison

--- drawingapp.js
+++ drawingapp.js
@@ -4,53 +4,84 @@ const decreaseBtn = document.getElementById("decrease");
 const sizeEl = document.getElementById("size");
 const colorEl = document.getElementById("color");
 const clearEl = document.getElementById("clear");
+const undoBtn = document.getElementById("undo");
+const redoBtn = document.getElementById("redo");
 
 const ctx = canvas.getContext("2d");
 
 let size = 10;
-let isPressed = false;
+let isDrawing = false;
 let color = "black";
 let x;
 let y;
+let drawing = [];
+let index = -1;
 
-canvas.addEventListener("mousedown", (e) => {
-  isPressed = true;
-  x = e.offsetX;
-  y = e.offsetY;
-});
-canvas.addEventListener("mouseup", (e) => {
-  isPressed = false;
-  x = undefined;
-  y = undefined;
-});
-canvas.addEventListener("mousemove", (e) => {
-  if (isPressed) {
-    const x2 = e.offsetX;
-    const y2 = e.offsetY;
-    drawCircle(x2, y2);
-    drawLine(x, y, x2, y2);
-    x = x2;
-    y = y2;
+function startPosition(e) {
+  isDrawing = true;
+  if (e.type === "touchstart") {
+    x = e.touches[0].clientX - canvas.getBoundingClientRect().left;
+    y = e.touches[0].clientY - canvas.getBoundingClientRect().top;
+  } else {
+    x = e.clientX - canvas.getBoundingClientRect().left;
+    y = e.clientY - canvas.getBoundingClientRect().top;
   }
-});
+  drawing.push([]);
+  index++;
+}
+
+function endPosition() {
+  isDrawing = false;
+}
+
+function draw(e) {
+  if (!isDrawing) return;
+
+  if (e.type === "touchmove") {
+    e.preventDefault();
+    x = e.touches[0].clientX - canvas.getBoundingClientRect().left;
+    y = e.touches[0].clientY - canvas.getBoundingClientRect().top;
+  } else {
+    x = e.clientX - canvas.getBoundingClientRect().left;
+    y = e.clientY - canvas.getBoundingClientRect().top;
+  }
+
+  drawCircle(x, y);
+  drawLine(x, y);
+}
+
+canvas.addEventListener("mousedown", startPosition);
+canvas.addEventListener("mouseup", endPosition);
+canvas.addEventListener("mousemove", draw);
+
+canvas.addEventListener("touchstart", startPosition);
+canvas.addEventListener("touchend", endPosition);
+canvas.addEventListener("touchmove", draw);
 
 function drawCircle(x, y) {
   ctx.beginPath();
   ctx.arc(x, y, size, 0, Math.PI * 2);
   ctx.fillStyle = color;
   ctx.fill();
+  drawing[index].push({ type: "circle", x, y, size, color });
 }
-function drawLine(x1, y1, x2, y2) {
+
+function drawLine(x, y) {
+  if (x === undefined || y === undefined) return;
+
   ctx.beginPath();
-  ctx.moveTo(x1, y1);
-  ctx.lineTo(x2, y2);
+  ctx.moveTo(x, y);
+  ctx.lineTo(x, y);
   ctx.strokeStyle = color;
   ctx.lineWidth = size * 2;
   ctx.stroke();
+  drawing[index].push({ type: "line", x, y, size, color });
 }
+
 function updateSizeOnScreen() {
   sizeEl.innerText = size;
 }
+
 increaseBtn.addEventListener("click", () => {
   size += 5;
   if (size > 50) {
@@ -58,6 +89,7 @@ increaseBtn.addEventListener("click", () => {
   }
   updateSizeOnScreen();
 });
+
 decreaseBtn.addEventListener("click", () => {
   size -= 5;
   if (size < 5) {
@@ -70,4 +102,34 @@ colorEl.addEventListener("change", (e) => (color = e.target.value));
 
 clearEl.addEventListener("click", () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawing = [];
+  index = -1;
 });
+
+undoBtn.addEventListener("click", () => {
+  if (index > 0) {
+    index--;
+    redraw();
+  }
+});
+
+redoBtn.addEventListener("click", () => {
+  if (index < drawing.length - 1) {
+    index++;
+    redraw();
+  }
+});
+
+function redraw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i <= index; i++) {
+    for (let j = 0; j < drawing[i].length; j++) {
+      const { type, x, y, size, color } = drawing[i][j];
+      if (type === "circle") {
+        drawCircle(x, y);
+      } else if (type === "line") {
+        drawLine(x, y);
+      }
+    }
+  }
+}
